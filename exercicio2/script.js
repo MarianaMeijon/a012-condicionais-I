4db1350dd859b72218247da6005ca63cbc2c2bb3
MarianaMeijon <marianameijon1989@gmail.com>
2022-09-06 12:08:00 -0300

    Exercicios 1 e 2 feitos.Faltando finalizar e executar o 3.

--- exercicio2/script.js
+++ exercicio2/script.js
@@ -1,9 +1,25 @@
-let idade;
-let isMaiorIdade;
-let isCursandoOutraFaculdade;
 
-if(){
+let idade = prompt ("Digite sua idade: ")
+let ensinoMedio = confirm ("Você terminou o ensino médio? ")
+let cursandoFaculdade = confirm ("Você está cursando alguma faculdade? ")
 
-}else{
-	
+if (idade >= 18) {
+    console.log (`Você já é maior de idade.`)
+} else {
+    console.log (`Você ainda é menor de idade.`)	
 }
+
+if (ensinoMedio === true) {
+    console.log (`Você já terminou de cursar o ensino médio.`)
+} else {
+    console.log (`Você ainda não terminou o ensino médio.`)	
+}
+
+if (cursandoFaculdade === false) {
+    console.log (`Você ainda não ingressou em uma faculdade.`)
+} else {
+    console.log (`Você já está cursando uma faculdade.`)	
+}
+
+
+
--- exercicio3/script.js
+++ exercicio3/script.js
@@ -1,1 +1,12 @@
-let nacionalidade //= adicione o prompt aqui
+
+const nacionalidade = prompt("Escreva aqui uma nacionalidade: ")
+
+if (nacionalidade == "brasileira" || nacionalidade == "argentina") {
+    console.log (nacionalidade)
+} else if (nacionalidade === "uruguaia" || nacionalidade === "chilena") {
+    console.log (nacionalidade)
+} else if (nacionalidade === "colombiana") {
+    console.log (nacionalidade)
+} else {
+    console.log ("Nacionalidade não encontrada.")
+}
